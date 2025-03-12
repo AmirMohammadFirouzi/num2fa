09e670640c8e7086dae893aca9519180b6d8c3d9
AmirMohammadFirouzi <amirMohammadFirouzi2007@gmail.com>
2025-03-12 17:22:00 +0330

    log off in fetchData

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -10,7 +10,7 @@ const fetchData = async (url) => {
     try {
         const response = await fetch(url);
         dataUnit = await response.json(); // Assign the parsed JSON object directly
-        console.log('Data loaded:', dataUnit);
+        // console.log('Data loaded:', dataUnit);
     } catch (error) {
         console.error('Error loading JSON:', error);
     }
